caeb9e1963ab3ea9d51a00bc96d9efd71f9e0143
Surhud More <surhud.more@ipmu.jp>
2015-11-15 14:24:36 +0900

    documentation added

--- tests.py
+++ tests.py
@@ -8,11 +8,11 @@ import savitzky_golay_werrors
 ###################################################################################################
 
 def main():
-	
+
 	testSine()
 	#testSineEqualWeights()
 	#testSpeed()
-	
+
 	return
 
 ###################################################################################################
@@ -26,23 +26,41 @@ def testSine():
 
 	xx = np.arange(0.0, 10.0, 0.01)
 	y_true = np.sin(xx)
-	
+
 	x = np.arange(0.0, 10.0, 0.2)
 	y = np.sin(x)
-	np.random.seed(152)
+	np.random.seed(250)
 	q_err = np.abs(np.random.normal(scatter_av, scatter_sigma, (len(x))))
 	for i in range(len(x)):
 		y[i] += np.random.normal(0.0, q_err[i])
 
 	sg = scipy.signal.savgol_filter(y, window, order, deriv = 0)
 	sg_err = savitzky_golay_werrors.savgol_filter_werror(y, window, order, q_err, deriv=None)
-	
-	plt.figure()
-	plt.errorbar(x, y, yerr = q_err, fmt = '.', marker = 'o', ms = 3.0, label = 'data')
-	plt.plot(xx, y_true, ':', label = 'True')
-	plt.plot(x, sg, '-', label = 'SG')
-	plt.plot(x, sg_err, '--', label = 'SG new')
-	plt.legend(loc = 3)
+
+	fig = plt.figure()
+        ax = fig.add_subplot(2, 1, 1)
+        import palettable
+        ax.set_color_cycle(palettable.colorbrewer.qualitative.Dark2_8.mpl_colors)
+
+	ax.errorbar(x, y, yerr = q_err, fmt = '.', marker = 'o', ms = 3.0,
+                label = 'Noisy data')
+	ax.plot(x, sg_err, '-', label = 'This work')
+	ax.plot(x, sg, '-', label = 'Traditional Savitzky-Golay filter')
+	ax.plot(xx, y_true, '-', label = 'Noiseless')
+	ax.legend(loc = 3, frameon=0, ncol=2)
+        ax.set_ylabel("y(x)")
+        ax.set_xticklabels([])
+
+        ax = fig.add_subplot(2, 1, 2)
+        ax.set_color_cycle(palettable.colorbrewer.qualitative.Dark2_8.mpl_colors)
+        ax.errorbar(x, y-np.sin(x), q_err, fmt = '.', marker = 'o', ms = 3.0)
+	ax.plot(x, sg_err-np.sin(x), '-', label = 'This work')
+        ax.plot(x, sg-np.sin(x), '-')
+        ax.axhline(0.0, color='grey')
+        ax.set_xlabel("x")
+        ax.set_ylabel("y-sin(x)")
+
+        plt.tight_layout()
 	plt.savefig('Test_Sine.pdf')
 
 	return
@@ -54,7 +72,7 @@ def testSineEqualWeights():
 	scatter = 0.1
 	window = 15
 	order = 4
-	
+
 	x = np.arange(0.0, 10.0, 0.2)
 	xx = np.arange(0.0, 10.0, 0.01)
 	y_true = np.sin(xx)
@@ -66,7 +84,7 @@ def testSineEqualWeights():
 
 	sg = scipy.signal.savgol_filter(y, window, order, deriv = 0)
 	sg_err = savitzky_golay_werrors.savgol_filter_werror(y, window, order, q_err, deriv=None)
-	
+
 	plt.figure()
 	plt.errorbar(x, y, yerr = q_err, fmt = '.', marker = 'o', ms = 3.0, label = 'data')
 	plt.plot(xx, y_true, ':', label = 'True')
@@ -80,12 +98,12 @@ def testSineEqualWeights():
 ###################################################################################################
 
 def testSpeed():
-	
+
 	scatter = 0.1
 	window = 15
 	order = 4
 	N = 1000
-	
+
 	x = np.arange(0.0, 10.0, 0.2)
 	y = np.sin(x)
 	np.random.seed(152)
@@ -97,12 +115,12 @@ def testSpeed():
 	for dummy in range(N):
 		_ = scipy.signal.savgol_filter(y, window, order, deriv = 0)
 	print('Numpy: %.2f s' % (time.clock() - t1))
-	
+
 	t1 = time.clock()
 	for dummy in range(N):
 		_ = savitzky_golay_werrors.savgol_filter_werror(y, window, order, q_err, deriv=None)
 	print('New:   %.2f s' % (time.clock() - t1))
-	
+
 	return
 
 ###################################################################################################
